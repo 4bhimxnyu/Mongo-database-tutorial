dde0fd30580f71e135aebe67c84b951a6cab9d97
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-12-08 03:39:42 +0530

    operations added

--- create.js
+++ create.js
@@ -19,3 +19,23 @@ db.Students.find().sort({name:1}).limit(1) // this method returns the first reco
 // update method in mongoDb
  
 db.Students.updateOne(filter , update) // syntax of updation of the data in database
+
+db.Students.updateOne({name:"abhi"},{$set:{fulltime:true}}) // this command updates the data in the database where the name is "abhi" and sets the full
+// $set parameter is used to set new value in the dataset
+
+db.Students.updateOne({name:"abhi"},{$unset:{fulltime:""}}) // unsets the fulltime field in the dataset where the name is "abhi"
+
+db.Students.updateMany({},{$set:{fulltime:false}}) // updates the value and sets the fulltime field to false in all the datasets in the database
+
+db.Students.updateMany({fulltime:{$exists:true}},{$set:{fulltime:""}})
+// this command first checks if the value "fulltime" exist in the data using $exists command and then updates the value and sets the fulltime field to empty string in all the datasets in the database.
+
+db.Students.deleteOne({name:"xyz"}) // deletes one data value as per the parameters passed
+
+db.Students.deleteMany({deletedDataSet:{$exists:false}})
+// this command first checks if the value "deletedDataSet" exist in the data using $exists command and then deletes the data value as per the parameters passed.
+
+db.Students.find({name:{$ne:"abhi"}})
+// this command finds all the data values where the name is not equal to "abhi" using $ne command. 
+
+// if 
