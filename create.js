7b965dc9100c8b3b75c498f2f2ad5832eee94c08
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-12-09 19:46:38 +0530

    not operator

--- create.js
+++ create.js
@@ -53,4 +53,9 @@ db.Students.find({$or:[{gender:"male"},{age:{$lte:45}}]})// here we are checking
 
 // NOR Condition
 db.Students.find({$nor:[{gender:"male"},{age:{$lte:45}}]}) // here we are checking the conditons and we are divinding the data based on that  **both the conditions should be excluded **
-// the end
+
+
+// not logical operator
+
+db.Students.find({age:{$not:{$lte:18}}}) 
+// this operator returns all the value which are not equal to the parameter , here in this case the database of the students have four value , out of which 3 are greater than 18 and 1 is less than 18 , so the output will be 3 value which are greater.
